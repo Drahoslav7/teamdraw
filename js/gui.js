ff4dbfa668fb1ea6f5cd55797763b12503923887
Drahoslav Bednář <drahoslav@yo2.cz>
2015-10-23 12:32:28 +0200

    gui upravy

--- js/gui.js
+++ js/gui.js
@@ -4,12 +4,17 @@ $(function () {
 	menu.visible = false;
 
 	menubutton.click(function () {
-	if (menu.visible) {
-		menu.animate({width: 0});
-		menu.visible = false;
-	} else	{
-		menu.animate({width: 300});
-		menu.visible = true;
-	}
+		if (menu.visible) {
+			menu.animate({width: 0},function(){
+				menubutton.html("menu ▶");
+			});
+			menu.visible = false;
+		} else	{
+			var width =  $("#top").width() - $("#menubutton").outerWidth() - $("#sharebutton").outerWidth();
+			menu.animate({width: width}, function(){
+				menubutton.html("menu ◀");
+			});
+			menu.visible = true;
+		}
 	});
 });
