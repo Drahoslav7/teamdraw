de126151b9eabea2cec834524dd9aa4052898697
David Balvín <l0g1n@seznam.cz>
2015-10-23 16:12:05 +0200

    Drobne upravky

--- js/gui.js
+++ js/gui.js
@@ -1,6 +1,6 @@
 $(function () {
 	var menubutton = $("#menubutton");
-	var sharebutton = $("#sharebutton")
+	var sharebutton = $("#sharebutton");
 	var menu = $("#menu");
 	menu.visible = false;
 
@@ -8,15 +8,15 @@ $(function () {
 		if (menu.visible) {
 			sharebutton.removeClass("btn-sharebutton-open");
 			menu.animate({width: 0},function(){
-				menubutton.html("menu ▶");
+				menubutton.html("Menu ▶");
 				menubutton.removeClass("btn-menubutton-open");
 			});
 			menu.visible = false;
 		} else	{
-			var width =  $("#top").width() - $("#menubutton").outerWidth() - $("#sharebutton").outerWidth();
+			var resWidth =  $("#top").width() - $("#menubutton").outerWidth() - $("#sharebutton").outerWidth();
 			menubutton.addClass("btn-menubutton-open");
-			menu.animate({width: width}, function(){
-				menubutton.html("menu ◀");
+			menu.animate({width: resWidth}, function(){
+				menubutton.html("Menu ◀");
 				sharebutton.addClass("btn-sharebutton-open");
 			});
 			menu.visible = true;
