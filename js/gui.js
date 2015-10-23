5bbb3c5a01b004545df128a0fa9f4cd05b867909
Drahoslav Bednář <drahoslav@yo2.cz>
2015-10-23 11:26:53 +0200

    merge sync

--- js/gui.js
+++ js/gui.js
@@ -1,15 +1,15 @@
 $(function () {
-  var menubutton = $("#menubutton");
-  var menu = $("#menu");
-  menu.visible = false;
+	var menubutton = $("#menubutton");
+	var menu = $("#menu");
+	menu.visible = false;
 
-  menubutton.click(function () {
-    if (menu.visible) {
-      menu.animate({width: 0});
-      menu.visible = false;
-    } else  {
-      menu.animate({width: 300});
-      menu.visible = true;
-    }
-  });
+	menubutton.click(function () {
+	if (menu.visible) {
+		menu.animate({width: 0});
+		menu.visible = false;
+	} else	{
+		menu.animate({width: 300});
+		menu.visible = true;
+	}
+	});
 });
