b7b5f41844d42ba1b5cb9915fe16c0b45f74b6a4
David Balvín <l0g1n@seznam.cz>
2015-10-23 12:59:35 +0200

    Barvičkyy

--- js/gui.js
+++ js/gui.js
@@ -1,18 +1,23 @@
 $(function () {
 	var menubutton = $("#menubutton");
+	var sharebutton = $("#sharebutton")
 	var menu = $("#menu");
 	menu.visible = false;
 
 	menubutton.click(function () {
 		if (menu.visible) {
+			sharebutton.removeClass("btn-sharebutton-open");
 			menu.animate({width: 0},function(){
 				menubutton.html("menu ▶");
+				menubutton.removeClass("btn-menubutton-open");
 			});
 			menu.visible = false;
 		} else	{
 			var width =  $("#top").width() - $("#menubutton").outerWidth() - $("#sharebutton").outerWidth();
+			menubutton.addClass("btn-menubutton-open");
 			menu.animate({width: width}, function(){
 				menubutton.html("menu ◀");
+				sharebutton.addClass("btn-sharebutton-open");
 			});
 			menu.visible = true;
 		}
